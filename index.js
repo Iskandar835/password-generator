2f40f5d2e33915ff357fe770a35bdd91ad4c4c43
Sacha Vandermoeten <sachadu835@gmail.com>
2025-06-10 17:05:52 +0200

    feat: chaque btn chiffre genere un password au click

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+// @ts-nocheck
+const CONSONANTS = "bcdfghjklmnpqrstvwxyz";
+const VOWELS = "aeiou";
 
+function getRandomCharacter(characters) {
+  return characters.charAt(Math.floor(Math.random() * characters.length));
+}
+
+function generateGroup(firstParam, secondParam) {
+  return (
+    getRandomCharacter(firstParam) +
+    getRandomCharacter(secondParam) +
+    getRandomCharacter(firstParam) +
+    getRandomCharacter(firstParam) +
+    getRandomCharacter(secondParam) +
+    getRandomCharacter(firstParam)
+  );
+}
+
+function generatePassword(groupsCount) {
+  const groups = [];
+  for (let i = 0; i < groupsCount; i++) {
+    groups.push(generateGroup(CONSONANTS, VOWELS));
+  }
+  return groups.join("-");
+}
+
+function actionClickingNumberBtn(id, groupsCount) {
+  const btn = document.getElementById(id);
+  const input = document.querySelector(".result-field");
+  btn?.addEventListener("click", () => {
+    input.value = generatePassword(groupsCount);
+  });
+}
+
+actionClickingNumberBtn("btn-one", 1);
+actionClickingNumberBtn("btn-two", 2);
+actionClickingNumberBtn("btn-three", 3);
